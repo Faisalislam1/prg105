ae2905e8bbbd7ee95ecc3fdbbfa5db43b2717bfa
Faisalislam1 <111308182+Faisalislam1@users.noreply.github.com>
2022-08-28 21:00:03 -0500

    Add files via upload

---
+++ Chapter 2 Practice 2.py
@@ -0,0 +1,30 @@
+#1 cup unsweetened almond milk
+#1 medium banana
+#1/2 cup frozen unsweetened blueberries
+#1/4 cup instant plain oatmeal
+#1 teaspoon maple syrup
+#1/2 teaspoon ground cinnamon
+#Dash sea salt
+#Find a simple recipe online, provide a link to the recipe in the comments. Display the ingredients with amounts and list the servings that will be made with the recipe. Ask the user to enter how many servings they would like to make, and display the required amount of ingredients they will need. Format to one decimal place.
+#https://www.tasteofhome.com/recipes/blueberry-pancake-smoothie/
+print("There will only be one smootie per Serving. 153 Calories per smootie")
+S = float(input("How Many Servings of Blueberry Pancake Smoothies would you like?" ))
+
+AM = float(1)*S #Almond Milk Cups
+BN = float(1)*S #Bananas
+BB = float(1/2)*S #Cups of frozen Blue Berrys
+OM = float(1/4)*S # Plain Oatmeal
+MPt = float(1)*S # Teaspoon of maple syrup
+CM = float(1/2) *S#Ground Cinnamon
+SS = ("Sea salt") #"DASH OF SEA SALT"
+
+
+
+
+print(" You will need ", AM,"Cups of Almond Milk")
+print(" You will need ", BN,"Medium Bananas")
+print(" You will need ", BB,"Cups of frozen Blue Berrys")
+print(" You will need ", OM,"Cups of instant Plain Oatmeal")
+print(" You will need ", MPt,"Teaspoon of maple syrup")
+print(" You will need ", CM,"Teaspoon of Ground Cinnamon")
+print(" You will need ",S,"Dash of Sea Salt")
