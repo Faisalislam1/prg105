a376fad0b0f55d57d647265de22a953e76585ea2
Faisalislam1 <111308182+Faisalislam1@users.noreply.github.com>
2022-09-21 13:15:58 -0500

    Update Chapter_4_Practice_2.py

--- Chapter_4_Practice_2.py
+++ Chapter_4_Practice_2.py
@@ -1,9 +1,10 @@
 
-
 n = 99
-while n > 0:
-    print(n, "Bottles of beer on the wall", n,"Bottles of beer on the wall.")
+while n > 1:
+    print(n, "Bottles of beer on the wall", n, "Bottles of beer on the wall.")
     n -= 1
-    print("Take one down and pass it around,", n,"bottles of beer on the wall")
+    print("Take one down and pass it around,", n, "bottles of beer on the wall")
+print("1 more bottle of beer on the wall, 1 more bottles of beer.")
+print("Take one down and pass it around, no more bottles of beer on the wall.")
 print("No more bottles of beer on the wall, no more bottles of beer.")
 print("Go to the store and buy some more, 99 bottles of beer on the wall.")
