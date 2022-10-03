144f7ee362ab70bc246d834e15fc209bfafaed36
Faisalislam1 <111308182+Faisalislam1@users.noreply.github.com>
2022-10-03 14:05:15 -0500

    Chapter 5 Assesment

---
+++ Chapter 5 Assessment Program.py
@@ -0,0 +1,30 @@
+
+
+
+def main():
+    print("This program will find the area of a shape for you.")
+    print("1. Rectangle")
+    print("2. Triangle")
+    print("3. Square")
+    print("4. Circle")
+    print("5. Quit")
+    n = int(input("When the "))
+    while
+
+
+def rectangle():
+
+
+
+def triangle():
+
+
+
+
+def square():
+
+
+
+def circle():
+
+
