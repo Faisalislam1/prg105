c298c3813d584890adf13ff235276772e9f22e81
Faisalislam1 <111308182+Faisalislam1@users.noreply.github.com>
2022-10-05 14:09:51 -0500

    Update Chapter 5 Assessment Program.py

--- Chapter 5 Assessment Program.py
+++ Chapter 5 Assessment Program.py
@@ -1,4 +1,4 @@
-
+import math
 
 
 def main():
@@ -8,23 +8,51 @@ def main():
     print("3. Square")
     print("4. Circle")
     print("5. Quit")
-    n = int(input("When the "))
-    while
+    number = int(input("What shape would you like to pick"))
+    while 0 < number and number > 5:
+        number = int(input("Enter renter whole number between 1 and 5: "))
+    if number == 1.00:
+        Rectangle()
+
+    elif number == 2.00:
+        Triangle()
+
+    elif number == 3.00:
+        Square()
+
+    elif number == 4.00:
+        Circle()
+
+    elif number == 5.00:
+        print("this is not a valid number \n Goodbye")
 
 
-def rectangle():
+def Rectangle():
+    width = float(input("Enter the width of the rectangle in cm:"))
+    hight = float(input("Enter the hight of the rectangle in cm"))
+    area = width*hight
+    print("Your area is", area)
 
 
-
-def triangle():
-
-
-
-
-def square():
+def Triangle():
+    width = float(input("Enter the width of the Triangle in cm:"))
+    hight = float(input("Enter the hight of the Triangle in cm"))
+    area = (width*hight)*.5
+    print("Your area is", area)
 
 
+def Square():
+    width = float(input("Enter the width of the rectangle in cm:"))
+    area = (width*width)
+    print("Your area is", area)
 
-def circle():
+
+def Circle():
+    radius = float(input("Enter the radius of Circle"))
+    area = math.pi*(radius*radius)
+    print("Your area is", area)
+
+main()
+
 
 
