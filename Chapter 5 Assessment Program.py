45c0cbb476630bcf8b7b262162d5cad8bf9360c2
Faisalislam1 <111308182+Faisalislam1@users.noreply.github.com>
2022-10-10 14:12:18 -0500

    Update Chapter 5 Assessment Program.py

--- Chapter 5 Assessment Program.py
+++ Chapter 5 Assessment Program.py
@@ -2,57 +2,60 @@ import math
 
 
 def main():
-    print("This program will find the area of a shape for you.")
-    print("1. Rectangle")
-    print("2. Triangle")
-    print("3. Square")
-    print("4. Circle")
-    print("5. Quit")
-    number = int(input("What shape would you like to pick"))
-    while 0 < number and number > 5:
-        number = int(input("Enter renter whole number between 1 and 5: "))
-    if number == 1.00:
-        Rectangle()
+    number = 0
+    while number != 5:
+        print("This program will find the area of a shape for you.")
+        print("1. Rectangle")
+        print("2. Triangle")
+        print("3. Square")
+        print("4. Circle")
+        print("5. Quit")
+        number = int(input("What shape would you like to pick"))
+        while 0 < number and number > 5:
+            number = int(input("Enter renter whole number between 1 and 5: "))
+        if number == 1.00:
+            width = float(input("Enter the width of the rectangle in cm:"))
+            height = float(input("Enter the height of the rectangle in cm"))
+            rectangle(width,height)
 
-    elif number == 2.00:
-        Triangle()
+        elif number == 2.00:
+            width = float(input("Enter the width of the Triangle in cm:"))
+            height = float(input("Enter the height of the Triangle in cm"))
+            triangle(width,height)
 
-    elif number == 3.00:
-        Square()
+        elif number == 3.00:
+            width = float(input("Enter the width of the rectangle in cm:"))
+            
+            square(width)
+    
+        elif number == 4.00:
+            radius = float(input("Enter the radius of Circle"))
+            
+            circle(radius)
 
-    elif number == 4.00:
-        Circle()
-
-    elif number == 5.00:
-        print("this is not a valid number \n Goodbye")
+        elif number == 5.00:
+            print("this is not a valid number \n Goodbye")
 
 
-def Rectangle():
-    width = float(input("Enter the width of the rectangle in cm:"))
-    hight = float(input("Enter the hight of the rectangle in cm"))
-    area = width*hight
+def rectangle(width,height):
+    area = width*height
     print("Your area is", area)
 
 
-def Triangle():
-    width = float(input("Enter the width of the Triangle in cm:"))
-    hight = float(input("Enter the hight of the Triangle in cm"))
-    area = (width*hight)*.5
+def triangle(width,height):
+    
+    area = (width*height)*.5
     print("Your area is", area)
 
 
-def Square():
-    width = float(input("Enter the width of the rectangle in cm:"))
+def square(width):
     area = (width*width)
     print("Your area is", area)
 
 
-def Circle():
-    radius = float(input("Enter the radius of Circle"))
+def circle(radius):
     area = math.pi*(radius*radius)
     print("Your area is", area)
 
+
 main()
-
-
-
